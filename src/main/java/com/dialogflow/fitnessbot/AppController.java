01a26523cb2b61cfb02b530b3a340213d0bb6bbc
Dima Karpukhin <dimadk787@gmail.com>
2020-08-06 22:55:28 +0300

    version 1

---
+++ src/main/java/com/dialogflow/fitnessbot/AppController.java
@@ -0,0 +1,4 @@
+package com.dialogflow.fitnessbot;
+
+public class AppController {
+}
---
+++ src/main/java/com/dialogflow/fitnessbot/FitnessbotApplication.java
@@ -0,0 +1,13 @@
+package com.dialogflow.fitnessbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessbotApplication.class, args);
+	}
+
+}
